c97e858a8d6d56af540791d7fc4870452f808a6d
Aleksandrova Olga <bulbenko@yandex.ru>
2024-01-25 20:27:50 +0100

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import { existsSync } from "fs";
+import path from 'path';
+import { fileURLToPath } from "url";
 const list = async () => {
-    // Write your code here 
+    const folderName = "files";
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const folderPath = path.join(__dirname, folderName);
+    
+  
+    if (!existsSync(folderPath) ) {
+      console.error("FS operation failed");
+      return;
+    }
+    
+    const files = await fs.readdir(folderPath);
+    for (const file of files) {
+      console.log(file);
+    }
 };
 
 await list();
