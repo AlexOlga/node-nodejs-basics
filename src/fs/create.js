df3ba5b878a533848a243992e4949db5509d2972
Aleksandrova Olga <bulbenko@yandex.ru>
2024-01-25 15:23:00 +0100

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Buffer } from "node:buffer";
+
+const DIR_NAME = "files";
+const FILE_NAME = "fresh.txt";
+const CONTENT = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, DIR_NAME, FILE_NAME);
+  if (fs.existsSync(filePath)) {
+    console.error("FS operation failed");
+    return;
+  }
+  const data = Buffer.from(CONTENT);
+  return fs.promises.writeFile(filePath, data);
 };
 
 await create();
