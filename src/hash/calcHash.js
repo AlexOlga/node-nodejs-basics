1bf1b2ae5c8a95135d160dfbe90b62a747d59f05
Aleksandrova Olga <bulbenko@yandex.ru>
2024-01-27 12:51:59 +0100

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = "fileToCalculateHashFor.txt";
+  const dirName = "files";
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, dirName, fileName);
+
+  const hash = crypto.createHash("sha256");
+  const stream = fs.createReadStream(filePath);
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  stream.on("end", () => {
+    const fileHash = hash.digest("hex");
+    console.log(`SHA256 Hash: ${fileHash}`);
+  });
 };
 
 await calculateHash();
