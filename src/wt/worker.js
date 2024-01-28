0c90244cd42a06483e5028292cb2ac690474eb4f
Aleksandrova Olga <bulbenko@yandex.ru>
2024-01-28 11:03:45 +0100

    fix: worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -2,15 +2,12 @@ import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
 // n should be received from main thread
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-
-const sendResult = (status, data) => {
-  parentPort.postMessage({ status, data });
-};
-parentPort.on("message", (data) => {
-  if (data && typeof data === "number") {
-    const result = nthFibonacci(data);
-    sendResult("resolved", result);
-  } else {
-    sendResult("error", null);
-  }
+ 
+const sendResult = () => {
+    
+    parentPort.on("message", (data) => { 
+             const result = nthFibonacci(data);
+    parentPort.postMessage({status: "resolved" , data: result });
 });
+};
+sendResult(); 
